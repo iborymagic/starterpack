38fbc2329a5347ee25e869b9e65d0c081959ec34
iborymagic <mlch8023@gmail.com>
2022-01-24 01:45:20 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './global.scss';
+
+import React from 'react';
+
+const App = () => {
+  return <div className="default">Typescript + React + SCSS + Webpack5</div>;
+};
+
+export default App;
